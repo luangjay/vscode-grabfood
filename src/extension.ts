b2ec086066ffbf220a0759080a60a4a7bbcdf75c
luangjay <luangjay.h@gmail.com>
2023-01-08 14:47:34 +0700

    Edit extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,51 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
+import * as path from 'path'
+import { spawn } from 'child_process'
+
+const rootPath = path.join(__dirname, '..')
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
+  await cmdSync('npm', ['install'])
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-grabfood" is now active!');
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Extension "vscode-grabfood" is now active!')
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-grabfood.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from GrabFood for VSCode!');
-	});
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  context.subscriptions.push(
+    vscode.commands.registerCommand('vscode-grabfood.orderFood', () => {
+      cmd('npm', ['run', 'start'])
+    })
+  )
+}
 
-	context.subscriptions.push(disposable);
+function cmd(command: string, args?: readonly string[]) {
+  let startProcess = spawn(command, args, {
+    shell: true,
+    cwd: rootPath,
+  })
+  startProcess.stdout.on('data', (data) => {
+    console.log(`${data}`)
+  })
+  startProcess.stderr.on('data', (data) => {
+    console.error(`${data}`)
+  })
+}
+
+function cmdSync(command: string, args?: readonly string[]) {
+  let startProcess = spawn(command, args, {
+    shell: true,
+    cwd: rootPath,
+  })
+  return new Promise((resolve) => {
+    startProcess.on('exit', (code) => {
+      resolve(code)
+    })
+  })
 }
 
 // This method is called when your extension is deactivated
